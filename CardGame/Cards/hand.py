7b10eb0ad3f12fa2cbee6ef958f8a428150f2605
Sean Casey <sccasey04@gmail.com>
2021-11-26 17:50:21 -0800

    Add getTotalPoints() method to Hand, fix constructor bug

--- CardGame/Cards/hand.py
+++ CardGame/Cards/hand.py
@@ -2,15 +2,24 @@ from card import Card
 
 class Hand():
     def __init__(self, cards: list[Card]) -> None:
-        if not isinstance(cards, list) or not isinstance(cards, tuple):
+        if not isinstance(cards, list) and not isinstance(cards, tuple):
             raise TypeError("'cards' must be list or tuple of Card type objects")
         self.cards = cards
     
-    def getValue(self) -> int:
-        pass
+    def getTotalPoints(self) -> int:
+        return sum([card.getValue() for card in self.cards])
 
     def discard(self):
         pass
 
     def addCard(self):
         pass
+
+
+if __name__ == "__main__":
+    card1 = Card("8", "spades")
+    card2 = Card("A", "diamonds")
+    card3 = Card("J", "clubs")
+
+    hand = Hand([card1, card2, card3])
+    print(hand.getTotalPoints())
