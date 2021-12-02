df99b0fb486914e9a718de4cb191e7770a356ffc
The0therChad <callmewheelz@gmail.com>
2021-12-02 15:25:44 -0800

    began work on player_test

--- tests/player_test.py
+++ tests/player_test.py
@@ -1,5 +1,15 @@
 import unittest
 from CardGame.Blackjack.person import Player
+from CardGame.Cards.deck import Deck
+
 
 class PlayerTest(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        return super().setUp()
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.p1 = Player()
+        cls.deck = Deck()
+        cls.deck.shuffle()
+        return super().setUpClass()
