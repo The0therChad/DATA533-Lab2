17648adc62288375d1bb5c11a47d1efb3c672242
Sean Casey <sccasey04@gmail.com>
2021-12-02 15:24:51 -0800

    Write a single test case to check card equality comparisons

--- tests/card_test.py
+++ tests/card_test.py
@@ -2,6 +2,27 @@ import unittest
 from CardGame.Cards.card import Card
 
 class CardTest(unittest.TestCase):
-    def test_card(self):
-        card = Card("A", "spades")
-        self.assertEqual(card.getValue(), 11)
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_card_equality(self):
+        self.assertEqual(CardTest.card1, CardTest.card3)
+        self.assertEqual(CardTest.card2, CardTest.card4)
+        self.assertNotEqual(CardTest.card2, CardTest.card1)
+        self.assertNotEqual(CardTest.card2, CardTest.card3)
+        self.assertNotEqual(CardTest.card4, CardTest.card1)
+    
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.card1 = Card("A", "spades")
+        cls.card2 = Card("J", "diamonds")
+        cls.card3 = Card("A", "spades")
+        cls.card4 = Card("J", "diamonds")
+    
+    @classmethod
+    def tearDownClass(cls) -> None:
+        return super().tearDownClass()
