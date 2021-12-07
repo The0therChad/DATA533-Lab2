9c102f60478cb7f8c00b5af59d1d7589afab9b43
Sean Casey <sccasey04@gmail.com>
2021-12-07 13:28:32 -0800

    Assemble all test cases into test suite

--- tests.py
+++ tests.py
@@ -7,6 +7,20 @@ from tests.dealer_test import DealerTest
 from tests.player_test import PlayerTest
 from tests.money_test import MoneyTest
 
+
+def run_tests():
+    suite = unittest.TestSuite()
+    test_classes = (
+        CardTest, DeckTest, HandTest, DealerTest, PlayerTest, MoneyTest
+    )
+    for test_class in test_classes:
+        suite.addTest(unittest.makeSuite(test_class))
+    runner = unittest.TextTestRunner(verbosity=2, buffer=True)
+    return runner.run(suite)
+    
+
+
 if __name__ == "__main__":
-    unittest.main(argv=[''], verbosity=2, buffer=True)
+    result = run_tests()
+    print("Success:", result.wasSuccessful())
     
