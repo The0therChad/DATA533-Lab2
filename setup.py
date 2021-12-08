4e1abb0ac46e35f3b295c5e8f696f4bae61228c2
The0therChad <87097613+The0therChad@users.noreply.github.com>
2021-12-08 11:01:23 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   version='1.0',
   packages=find_packages(exclude=['tests*']),
   license='MIT',
-  description='A python package to generate playing cards and use them to play a game of Blackjack in the terminal.’,
+  description='A python package to generate playing cards and use them to play a game of Blackjack in the terminal.',
   url='https://github.com/The0therChad/DATA533-Lab3.git',
   author='Sean-C-Casey & The0therChad',
   author_email='cjwheelz@student.ubc.ca'
